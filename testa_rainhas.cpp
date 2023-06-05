efa8e236b2d02d3de3524a36e55fbd006762b708
TagFernandes <tagfernandes@hotmail.com>
2023-06-05 13:22:41 -0400

    teste_2

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -26,6 +26,7 @@ TEST_CASE("solucoes invalidas para o problema", "[rainhas]") {
     REQUIRE(verifica_solucao_8_rainhas("teste_Beta5.txt") == 0);
 
     REQUIRE(verifica_solucao_8_rainhas("teste_1.txt") == 0);
+    REQUIRE(verifica_solucao_8_rainhas("teste_2.txt") == 0);
 }
 
 /**
