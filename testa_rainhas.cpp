93a547c6650935c2b6209955d4a0b4f95b3cdbb9
TagFernandes <tagfernandes@hotmail.com>
2023-06-05 13:43:35 -0400

    teste_12

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -58,6 +58,6 @@ TEST_CASE("solucoes validas para o problema", "[rainhas]") {
 *                 -2 caso não seja possível abrir o arquivo.
 */
 TEST_CASE("matriz invalida para o problema", "[rainhas]") {
-    REQUIRE(verifica_solucao_8_rainhas("teste_Beta2.txt") == -1);
+    REQUIRE(verifica_solucao_8_rainhas("teste_12.txt") == -1);
     REQUIRE(verifica_solucao_8_rainhas("teste_Beta6.txt") == -1);
 }
